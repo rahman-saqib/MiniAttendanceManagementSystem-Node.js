bd66e8a0973e346af065b82e6b8ec36693dc3772
unknown <rahmansaqib17@gmail.com>
2020-04-14 00:49:58 +0530

    making it better

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
+const { stringify } = require('querystring');
+const app = express();
+var port = process.env.PORT || 8080
+app.use(bodyParser.json());
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb+srv://Sam:softaweb@cluster0-xe3q0.mongodb.net/test?retryWrites=true&w=majority';
+
+
+app.get('/form', (_, res) => res.sendFile(`${__dirname}/sites/index.html`));
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.post('/thankyou', async (req, res) => {
+    console.log(req.body);
+    res.sendFile(`${__dirname}/sites/success.html`);
+    MongoClient.connect(url, function(err , db){
+        var q = req.body;
+        var dbo = db.db("AttendanceData");
+        console.log(q);
+        if(err) throw err;
+        dbo.collection('stuattendance').insertOne(q, function(err , result){
+            if(err) throw err;
+            
+            console.log("1 Data inserted success");
+            db.close();
+        });
+
+    });
+
+    
+  });
+
+app.listen(port, () => console.log('Server started on port'));
