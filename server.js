74f59877bcdc9616636862b6790ca42bfe8060df
unknown <rahmansaqib17@gmail.com>
2020-04-29 21:46:11 +0530

    nice

--- server.js
+++ server.js
@@ -4,14 +4,15 @@ const rateLimit = require('express-rate-limit');
 const xss = require("xss-clean");
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize') 
+
 //const fetch = require('node-fetch');
 //const { stringify } = require('querystring');
 const app = express();
-var port = process.env.PORT
+var port = process.env.PORT || 8080
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 var MongoClient = require('mongodb').MongoClient;
-var url = 'mongodb+srv://Sam:<password>@cluster0-xe3q0.mongodb.net/test?retryWrites=true&w=majority';
+var url = 'mongodb+srv://Sam:softaweb@cluster0-xe3q0.mongodb.net/test?retryWrites=true&w=majority';
 //var Recaptcha = require('express-recaptcha').RecaptchaV3;
 //sfsgdfgdgdgddgdg
 app.use(express.json({ limit: '10kb' }));
@@ -24,22 +25,24 @@ app.use('/form', limit);
 app.use(xss());
 app.use(helmet());
 app.use(mongoSanitize());
-app.get('/form', (_, res) => res.sendFile(`${__dirname}/sites/index.html`));
+app.get('/form', (req, res) => res.sendFile(`${__dirname}/sites/index.html`));
 
 
-app.post('/thankyou', async (req, res) => {
+app.post('/thankyou', (req, res) => {
     //console.log(req.body);
     res.sendFile(`${__dirname}/sites/success.html`);
-    MongoClient.connect(url, function(err , db){
+    MongoClient.connect(url, function(err , client){
+         if(err) throw err;
         var q = req.body;
-        var dbo = db.db("AttendanceData");
+        var db;
+        const dbo = client.db("AttendanceData");
         console.log(q);
-        if(err) throw err;
+       
         dbo.collection('stuattendance').insertOne(q, function(err , result){
             if(err) throw err;
             
             console.log("1 Data inserted success");
-            db.close();
+            client.close();
         });
 
     });
@@ -48,3 +51,5 @@ app.post('/thankyou', async (req, res) => {
   });
 
 app.listen(port, () => console.log('Server started on port'));
+
+
